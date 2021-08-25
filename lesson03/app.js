69e67b9a5e380ebc66ec0798e89fa046b9e06578
yarikkk13 <yaroslav.mahas@gmail.com>
2021-08-25 19:22:42 +0300

    eslinted_first

--- lesson03/app.js
+++ lesson03/app.js
@@ -2,67 +2,65 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 
-const {PORT} = require('./configs/config');
-const {CREATE, NOT_FOUND} = require('./configs/status.codes.enum');
-const {readFile, writeFile} = require('./helpers/async');
-
+const { PORT } = require('./configs/config');
+const { CREATE, NOT_FOUND } = require('./configs/status.codes.enum');
+const { readFile, writeFile } = require('./helpers/async');
 
 const app = express();
 const staticPath = path.join(__dirname, 'static');
 const usersPath = path.join(__dirname, 'database', 'users.json');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(staticPath));
 
 app.set('view engine', '.hbs');
-app.engine('.hbs', handlebars({defaultLayout: false}));
+app.engine('.hbs', handlebars({ defaultLayout: false }));
 app.set('views', staticPath);
 
 let loginned = false;
 
-//logination1
+// logination1
 app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    const getUserByEmail = async (email) => {
+    const { email, password } = req.body;
+    const getUserByEmail = async (email1) => {
         const getUser = await readFile(usersPath);
         const user = JSON.parse(getUser);
-        return await user.find(user => user.email === email);
-    }
+        return user.find((user1) => user1.email === email1);
+    };
     const user = await getUserByEmail(email);
     if (!user) {
         return res.status(NOT_FOUND).redirect('/registering');
     }
     if (user.password !== password) {
         return res.status(NOT_FOUND)
-            .json({"message": "Check your password"})
-    } else {
-        loginned = true
-        return res.redirect('/users');
+            .json({ message: 'Check your password' });
     }
+    loginned = true;
+    return res.redirect('/users');
 });
 
-//registering
+// registering
 app.post('/registering', async (req, res) => {
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
-    const getUserByEmail = async (email) => {
+    const getUserByEmail = async (email1) => {
         const getUser = await readFile(usersPath);
         const user = JSON.parse(getUser);
-        return await user.find(user => user.email === email);
-    }
+        return user.find((user1) => user1.email === email1);
+    };
     const user = await getUserByEmail(email);
     if (!user) {
-        const addUsers = async (user) => {
+        const addUsers = async (user1) => {
             const getAllUsers = async () => {
                 const getUser = await readFile(usersPath);
                 return JSON.parse(getUser);
-            }
+            };
             const users = await getAllUsers();
-            users.push(user);
+            users.push(user1);
             await writeFile(usersPath, JSON.stringify(users));
-        }
-        await addUsers({name, email, password});
+        };
+        await addUsers({ name, email, password });
         return res.status(CREATE).redirect('/login');
     }
     return res.redirect('/login');
@@ -70,23 +68,21 @@ app.post('/registering', async (req, res) => {
 // render endpoints
 
 app.get('/login', (req, res) => {
-    res.render('login', {loginned})
-})
-
-app.get('/registering', (req, res) => {
-    return res.render('register');
+    res.render('login', { loginned });
 });
+
+app.get('/registering', (req, res) => res.render('register'));
 
 app.get('/users', async (req, res) => {
     const getAllUsers = async () => {
         const getUser = await readFile(usersPath);
         return JSON.parse(getUser);
-    }
-    let users = await getAllUsers()
-    return res.render('users', {users});
-})
+    };
+    const users = await getAllUsers();
+    return res.render('users', { users });
+});
 
 // application
 app.listen(PORT, () => {
-    console.log('app listen port', PORT)
-})
+//    console.log('app listen port', PORT);
+});
--- lesson03/configs/status.codes.enum.js
+++ lesson03/configs/status.codes.enum.js
@@ -3,4 +3,4 @@ module.exports = {
     BAD_REQUEST: 400,
     UNAUTHORIZED: 401,
     NOT_FOUND: 404,
-}
+};
