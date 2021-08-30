11cf4638dbb2c354d09f1213d683886e7a6ce4c5
yarikkk13 <yaroslav.mahas@gmail.com>
2021-08-30 22:48:58 +0300

    car_validator+index+constant

--- lesson05/validators/car.validator.js
+++ lesson05/validators/car.validator.js
@@ -1,17 +1,17 @@
 const Joi = require('joi');
 
-const { CURRENT_YEAR } = require('../configs/constants');
+const { CURRENT_YEAR, YEAR_OF_FIRST_CAR } = require('../configs/constants');
 
 module.exports = {
     createCarValidator: Joi.object({
         brand: Joi.string().trim().required(),
-        year: Joi.number().integer().min(1886).max(CURRENT_YEAR)
+        year: Joi.number().integer().min(YEAR_OF_FIRST_CAR).max(CURRENT_YEAR)
             .required(),
         model: Joi.string().trim()
     }),
     updateCarValidator: Joi.object({
         brand: Joi.string().trim(),
-        year: Joi.number().integer().min(1886).max(CURRENT_YEAR),
+        year: Joi.number().integer().min(YEAR_OF_FIRST_CAR).max(CURRENT_YEAR),
         model: Joi.string().trim()
     }),
 };
